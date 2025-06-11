3d4e03049792a169eda2c3fa06cd8d8503d8346c
saritakarwaa <karwasarita672@gmail.com>
2025-06-11 21:20:27 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SidebarDemo from "@/components/sidebar-demo";
+
+function App() {
+
+  return (
+    <div className='w-screen h-full overflow-x-hidden'>
+      <SidebarDemo />
+    </div>
+  )
+}
+
+export default App
